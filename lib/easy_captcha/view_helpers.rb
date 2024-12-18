7a9f3d3cbb671c9ec35d6b14f7273e8dc447a3d5
shinHeYu <robin1215@4point-inc.com>
2024-12-18 15:10:10 +0800

    feat: captcha_tag 的圖片url 提出

--- lib/easy_captcha/view_helpers.rb
+++ lib/easy_captcha/view_helpers.rb
@@ -2,10 +2,10 @@ module EasyCaptcha
   # helper class for ActionView
   module ViewHelpers
     # generate an image_tag for captcha image
-    def captcha_tag(*args)
+    def captcha_tag(captcha_image_path = captcha_path(:i => Time.now.to_i), custom_options = {}) 
       options = { :alt => 'captcha', :width => EasyCaptcha.image_width, :height => EasyCaptcha.image_height }
-      options.merge! args.extract_options!
-      image_tag(captcha_path(:i => Time.now.to_i), options)
+      options.merge! custom_options
+      image_tag(captcha_image_path , options)
     end
   end
 end
